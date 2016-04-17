978e149eb782d43150a7b4ff7ebaadf0ed7ccf18
DanyalXD <danyal1995@hotmail.co.uk>
2016-04-17 09:58:22 +0100

    j

--- js/checkEmotionLevel.js
+++ js/checkEmotionLevel.js
@@ -6,28 +6,37 @@ function checkEmotionLevel() {
 			switch(i){
 			case 0:
 				document.body.style.background = "#A6D7E1";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Azzazzy%3Aplaylist%3A36CFhzHzK8YW3mtPwXT3y0";
 				break;
 			case 1:
 				document.body.style.background = "#90CDDA";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Adanyalc95%3Aplaylist%3A3aa3jGMKLywujtAuJpGL4a";
 				break;
 			case 2:
 				document.body.style.background = "#79C3D2";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Adanyalc95%3Aplaylist%3A6x1R9eyNJTQmXaQKc4cfYX";
 				break;
 			case 3:
 				document.body.style.background = "#63B9CB";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Adanyalc95%3Aplaylist%3A6x1R9eyNJTQmXaQKc4cfYX";
 				break;
 			case 4:
 				document.body.style.background = "#A6D7E1";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Adanyalc95%3Aplaylist%3A55Vvs3Gc6QHVA0UWQJF2IT";
 				break;
 			case 5:
 				document.body.style.background = "#90CDDA";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Azzazzy%3Aplaylist%3A36CFhzHzK8YW3mtPwXT3y0";
 				break;
 			case 6:
 				document.body.style.background = "#79C3D2";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Azzazzy%3Aplaylist%3A36CFhzHzK8YW3mtPwXT3y0";
 				break;
 			case 7:
 				document.body.style.background = "#63B9CB";
+				document.getElementById('spotify').src ="https://embed.spotify.com/?uri=spotify%3Auser%3Azzazzy%3Aplaylist%3A36CFhzHzK8YW3mtPwXT3y0";
 				break;
 			}
 	}
 }
+
